9462991c413a3ac1f64f200e207f434df541d16e
Sergey Ivanov <Reger21a@yandex.ru>
2024-04-05 11:46:09 +0300

    Добвить классы User и film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    long id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Duration duration;
 }
